8e7253b5f1a96cc1922ec0d57cf94c264d035721
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-07-31 18:38:06 -0700

    added pages property

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -3,8 +3,18 @@ import {connect} from 'react-redux';
 
 class BookDetail extends Component{
   render(){
+    //when we first boot the app up, this.props.book is null. If we enter this case, we're going to return early from the render method. Once they actually click on a book, it's going to update our application state, which is going to cause our component to rerender. When it rerenders, we will then have this.props.book defined, which means we will successfully get down to the next block of code
+
+    if(!this.props.book){
+      return <div>Select a book to get started.</div>
+    }
+
     return (
-      <div>Book Detail!</div>
+      <div>
+        <h3>Details for:</h3>
+        <div>Title: {this.props.book.title}</div>
+        <div>Pages: {this.props.book.pages}</div>
+      </div>
     )
   }
 }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -2,9 +2,9 @@
 //two step process: 1. create the reducer 2. wire it into the application
 export default function(){
   return [
-    {title: 'JavaScript: The Good Parts'},
-    {title: 'Harry Potter'},
-    {title: 'The Dark Tower'},
-    {title: 'Eloquent Ruby'},
+    {title: 'JavaScript: The Good Parts', pages: 101},
+    {title: 'Harry Potter', pages: 89},
+    {title: 'The Dark Tower', pages: 35},
+    {title: 'Eloquent Ruby', pages: 1},
   ]
 }
